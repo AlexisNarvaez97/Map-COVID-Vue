e307251a800b300f2d2c3927933b41f864d7c232
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-03-31 22:04:04 -0600

    Add map by google maps api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TravelMap class="travel-map"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TravelMap from "./components/TravelMap";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TravelMap
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.travel-map {
+  height: 100vh;
+  width: 100vw;
 }
 </style>
---
+++ src/components/TravelMap.vue
@@ -0,0 +1,76 @@
+<template>
+  <GoogleMapLoader
+    :mapConfig="mapConfig"
+    apiKey="AIzaSyBmV8I1mjdduDK0gsbRMq-ObtR207URdBo"
+  >
+  // insert your google maps api key to render styled map
+    <!-- <template slot-scope="{ google, map }">
+      <GoogleMapMarker
+        v-for="marker in markers"
+        :key="marker.id"
+        :marker="marker"
+        :google="google"
+        :map="map"
+      />
+    </template> -->
+  </GoogleMapLoader>
+</template>
+
+<script>
+import GoogleMapLoader from "./GoogleMapLoader";
+// import GoogleMapMarker from "./GoogleMapMarker";
+// import GoogleMapLine from "./GoogleMapLine";
+
+
+export default {
+  components: {
+    GoogleMapLoader,
+    // GoogleMapMarker
+  },
+
+  data() {
+    return {
+      markers: [
+        {
+          id: "a",
+          position: { lat: 3, lng: 101 }
+        },
+        {
+          id: "b",
+          position: { lat: 5, lng: 99 }
+        },
+        {
+          id: "c",
+          position: { lat: 6, lng: 97 }
+        }
+      ],
+      lines: [
+        {
+          id: "1",
+          path: [{ lat: 3, lng: 101 }, { lat: 5, lng: 99 }]
+        },
+        {
+          id: "2",
+          path: [{ lat: 5, lng: 99 }, { lat: 6, lng: 97 }]
+        }
+      ]
+    };
+  },
+
+  computed: {
+    mapConfig() {
+      return {
+        center: {
+          lat: 23.6345005,
+          lng: -102.5527878
+        },
+        zoom: 5
+      };
+    },
+
+    mapCenter() {
+      return this.markers[1].position;
+    }
+  }
+};
+</script>
